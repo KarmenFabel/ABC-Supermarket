af5fc9d2e193e70e11ad213d780c5afb9fb1bbd4
Karmen Fabel <karmenfabel@gmail.com>
2022-01-10 14:38:59 +0200

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ABC.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:GridView ID="gvABC" runat="server" AutoGenerateColumns="False" ShowFooter="True" 
+                ShowHeaderWhenEmpty="True"
+
+                OnRowCommand="gvABC_RowCommand" OnRowEditing="gvABC_RowEditing" OnRowCancelingEdit="gvABC_RowCancelingEdit"
+                OnRowUpdating="gvABC_RowUpdating" OnRowDeleting="gvABC_RowDeleting"
+
+                BackColor="#DEBA84" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px" CellPadding="3" OnSelectedIndexChanged="gvABC_SelectedIndexChanged" CellSpacing="2">
+                <%-- Theme Properties --%>
+                <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+                <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" />
+                <PagerStyle ForeColor="#8C4510" HorizontalAlign="Center" />
+                <RowStyle ForeColor="#8C4510" BackColor="#FFF7E7" />
+                <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#FFF1D4" />
+                <SortedAscendingHeaderStyle BackColor="#B95C30" />
+                <SortedDescendingCellStyle BackColor="#F1E5CE" />
+                <SortedDescendingHeaderStyle BackColor="#93451F" />
+                
+                <Columns>
+                    <asp:TemplateField HeaderText="Name">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Name") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtName" Text='<%# Eval("Name") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtNameFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Details">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Details") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtDetails" Text='<%# Eval("Details") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtDetailsFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Price">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Price") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtContact" Text='<%# Eval("Price") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtPriceFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                   
+                       
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:ImageButton ImageUrl="~/Images/edit.png" runat="server" CommandName="Edit" ToolTip="Edit" Width="20px" Height="20px"/>
+                            <asp:ImageButton ImageUrl="~/Images/delete.png" runat="server" CommandName="Delete" ToolTip="Delete" Width="20px" Height="20px"/>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:ImageButton ImageUrl="~/Images/save.png" runat="server" CommandName="Update" ToolTip="Update" Width="20px" Height="20px"/>
+                            <asp:ImageButton ImageUrl="~/Images/cancel.png" runat="server" CommandName="Cancel" ToolTip="Cancel" Width="20px" Height="20px"/>
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:ImageButton ImageUrl="~/Images/addnew.png" runat="server" CommandName="AddNew" ToolTip="Add New" Width="20px" Height="20px"/>
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                </Columns>
+            </asp:GridView>
+            <br />
+            <asp:Label ID="lblSuccessMessage" Text="" runat="server" ForeColor="Green" />
+            <br />
+            <asp:Label ID="lblErrorMessage" Text="" runat="server" ForeColor="Red" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+//References: CodAffection, Youtube, C# Asp.Net Gridview- Insert Update and Delete With SQL Server
+//https:www.youtube.com/watch?v=vuoJeQ4L3Q4L3Wl&ab_channel=CodAffection
+//https://abc20104675.azurewebsites.net
+namespace ABC
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        //Connecting the Azure database to the Application
+        string connectionString = "Server=tcp:dbsvccldv621220104675.database.windows.net,1433;" +
+            "Initial Catalog=DB_VC_CLDV6212_20104675;Persist Security Info=False;User ID=kfcldv20104675;Password=Alice@321;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //If statement to populate gridview
+            if (!IsPostBack)
+            {
+                PopulateGridview();
+            }
+        }
+        //Class to populate gridview
+        void PopulateGridview()
+        {
+            DataTable dtbl = new DataTable();
+            //connection string
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                //All information of Data retrieved from database
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM Product", sqlCon);
+               //fill gridview with database information
+                sqlDa.Fill(dtbl);
+            }
+            if (dtbl.Rows.Count > 0)
+            {
+                gvABC.DataSource = dtbl;
+                gvABC.DataBind();
+            }
+            else
+            {
+                //If there is no data to fill gridview
+                dtbl.Rows.Add(dtbl.NewRow());
+                gvABC.DataSource = dtbl;
+                //problem
+                gvABC.DataBind();
+                gvABC.Rows[0].Cells.Clear();
+                gvABC.Rows[0].Cells.Add(new TableCell());
+                gvABC.Rows[0].Cells[0].ColumnSpan = dtbl.Columns.Count;
+                gvABC.Rows[0].Cells[0].Text = "No Data Found ..!";
+                gvABC.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+            }
+
+        }
+        //To addnew item to table
+        protected void gvABC_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName.Equals("AddNew"))
+                {
+                    using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                    {
+                        sqlCon.Open();
+                        string query = "INSERT INTO Product (Name,Details,Price) VALUES (@Name,@Details,@Price)";
+                        SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                        sqlCmd.Parameters.AddWithValue("@Name", (gvABC.FooterRow.FindControl("txtNameFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Details", (gvABC.FooterRow.FindControl("txtDetailsFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Price", (gvABC.FooterRow.FindControl("txtPriceFooter") as TextBox).Text.Trim());
+                        sqlCmd.ExecuteNonQuery();
+                        PopulateGridview();
+                        lblSuccessMessage.Text = "New Record Added";
+                        lblErrorMessage.Text = "";
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+        //editing items in table
+        protected void gvABC_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvABC.EditIndex = e.NewEditIndex;
+            PopulateGridview();
+        }
+        //to cancell editing items
+        protected void gvABC_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvABC.EditIndex = -1;
+            PopulateGridview();
+        }
+            //updating the row
+        protected void gvABC_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+
+                    sqlCon.Open();
+                    string query = "UPDATE Product SET Name=@Name,Details=@Details,Price=@Price WHERE id = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@Name", (gvABC.Rows[e.RowIndex].FindControl("txtName") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Details", (gvABC.Rows[e.RowIndex].FindControl("txtDetails") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Price", (gvABC.Rows[e.RowIndex].FindControl("txtPrice") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@id",Convert.ToInt32(gvABC.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    gvABC.EditIndex = -1;
+                    PopulateGridview();
+                    lblSuccessMessage.Text = "Selected Record Updated";
+                    lblErrorMessage.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+            //Delete a row
+        protected void gvABC_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    string query = "DELETE FROM Product WHERE ID = @ID";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@ID", Convert.ToInt32(gvABC.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    PopulateGridview();
+                    lblSuccessMessage.Text = "Selected Record Deleted";
+                    lblErrorMessage.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+
+
+        protected void gvABC_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+       
+    }
+}
